b3a86ab3b1aa5b70c142c750996aff0e8030c2c5
Avijit129 <www.avijitb@gmail.com>
2025-12-15 14:10:39 +0600

    resolve git hub alert issue

--- resources/js/MetaData.js
+++ resources/js/MetaData.js
@@ -2,8 +2,7 @@ const metaData = {
     title: 'Your Website Title',
     description: 'Your website description...',
     appUrl: 'http://127.0.0.1:8000/',
-    openRouter: 'https://openrouter.ai/api/v1/chat/completions',
-    openRouterToken: 'Bearer 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'
+    
     // Other metadata properties
   };
   export default metaData;
